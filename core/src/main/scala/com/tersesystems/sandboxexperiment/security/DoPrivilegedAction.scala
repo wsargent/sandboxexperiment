3a75e252ee67aa9df9418051320d3327ce1ad9ff
Will Sargent <will.sargent@gmail.com>
2015-12-30 18:14:50 -0800

    Add check for null acc

--- core/src/main/scala/com/tersesystems/sandboxexperiment/security/DoPrivilegedAction.scala
+++ core/src/main/scala/com/tersesystems/sandboxexperiment/security/DoPrivilegedAction.scala
@@ -2,11 +2,20 @@ package com.tersesystems.sandboxexperiment.security
 
 import java.security.{AccessControlContext, Permission, PrivilegedAction}
 
-
-// https://docs.oracle.com/javase/7/docs/technotes/guides/security/doprivileged.html
 object DoPrivilegedAction {
-
   def apply[T](permissions: Permission*)(block: => T)(implicit context: AccessControlContext): T = {
+    // https://docs.oracle.com/javase/7/docs/technotes/guides/security/doprivileged.html
+    //
+    // http://www.oracle.com/technetwork/java/seccodeguide-139067.html#9
+    // Guideline 9-3 / ACCESS-3: Safely invoke java.security.AccessController.doPrivileged
+    //
+    // The two-argument overloads of doPrivileged allow changing of privileges to that of a previous acquired context.
+    // A null context is interpreted as adding no further restrictions. Therefore, before using stored contexts,
+    // make sure that they are not null (AccessController.getContext never returns null).
+
+    if (context == null) {
+      throw new SecurityException("Missing AccessControlContext")
+    }
     val action = new DoPrivilegedAction[T](block)
     java.security.AccessController.doPrivileged(action, context, permissions: _*)
   }
