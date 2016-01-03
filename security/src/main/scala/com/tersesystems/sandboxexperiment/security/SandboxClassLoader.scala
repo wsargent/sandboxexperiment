17e607a00355b691fcb810e05d5dd667e5555117
Will Sargent <will.sargent@gmail.com>
2016-01-02 21:02:37 -0800

    Forbid various classes that have weaker checking or are horribly dangerous.

--- security/src/main/scala/com/tersesystems/sandboxexperiment/security/SandboxClassLoader.scala
+++ security/src/main/scala/com/tersesystems/sandboxexperiment/security/SandboxClassLoader.scala
@@ -17,5 +17,51 @@ import scala.collection.JavaConverters._
   * There's some SBT BuildInfo magic in there that points to the file URL that the sandbox
   * is located in, so we don't have to explicitly jar it up.
   */
-class SandboxClassLoader(parent: ClassLoader) extends URLClassLoader(BuildInfo.sandbox.toList.toArray, parent)
+class SandboxClassLoader(parent: ClassLoader) extends URLClassLoader(BuildInfo.sandbox.toList.toArray, parent) {
 
+  import SandboxClassLoader._
+
+  override def loadClass(name: String, resolve: Boolean): Class[_] = {
+    // println(s"loadClass: name = ${name}, resolve = $resolve")
+
+    if (forbiddenClasses.contains(name)) {
+      throw new IllegalArgumentException("This functionality is disabled")
+    }
+    super.loadClass(name, resolve)
+  }
+
+  override def findClass(name: String): Class[_] = {
+    super.findClass(name)
+  }
+}
+
+
+object SandboxClassLoader {
+
+  // There are various classes that have weaker security privileges and only check the immediate parent.
+  // The simplest way to deal with them is to not let them be instantiated in the first place.
+
+  val miscClasses =
+    """java.io.ObjectInputStream
+      |java.io.ObjectOutputStream
+      |java.io.ObjectStreamField
+      |java.io.ObjectStreamClass
+      |java.util.logging.Logger
+      |java.sql.DriverManager
+      |javax.sql.rowset.serial.SerialJavaObject
+    """.stripMargin.split("\n").toSet
+
+  // a bit extreme, but see http://www.security-explorations.com/materials/se-2014-02-report.pdf
+  val javaClasses =
+    """java.lang.Class
+      |java.lang.ClassLoader
+      |java.lang.Package
+      |java.lang.invoke.MethodHandleProxies
+      |java.lang.reflect.Proxy
+      |java.lang.reflect.Constructor
+      |java.lang.reflect.Method
+    """.stripMargin.split("\n").toSet
+
+  val forbiddenClasses: Set[String] = javaClasses ++ miscClasses
+
+}
